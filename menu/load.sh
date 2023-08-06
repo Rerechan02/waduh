1dac1ee980afc7033a281fc87f275ef906b430a8
Rerechan02 <122332019+Rerechan02@users.noreply.github.com>
2023-08-06 19:32:33 +0700

    Add files via upload

---
+++ menu/load.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+spinner()
+{
+    local pid=$!
+    local delay=0.1
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
+echo "Proses sedang berjalan..."
+sleep 1 & spinner
+echo "Proses selesai."
